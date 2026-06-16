fbec806b3f9dfb180a3d4672db148867c1125952
Sameer-472 <muhammadsameer2016@yahoo.com>
2026-06-16 13:29:28 +0500

    added git graph

--- app/page.js
+++ app/page.js
@@ -19,7 +19,7 @@ export default function Home() {
     <main className="flex-1">
       <section className="px-6 py-4 space-y-16">
         <Profile />
-        {/* <GitHubGraph /> */}
+        <GitHubGraph />
         <Experience />
         <FeatureProjects />
         {/* <Timeline /> */}
